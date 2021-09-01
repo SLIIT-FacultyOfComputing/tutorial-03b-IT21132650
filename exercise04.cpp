c87134afbcafa31ac187050b62e28bea384effd6
IT21132650 <87793882+IT21132650@users.noreply.github.com>
2021-09-01 12:29:53 +0000

    Ex 4 tutorial 3

--- exercise04.cpp
+++ exercise04.cpp
@@ -1,21 +1,41 @@
 #include <iostream>
+
 using namespace std;
+int volume(int height, int width, int length);
 
-void print(int len, int wth);
-void input(int len, int wth);
+struct Box{
+	int Height;
+	int Width;
+	int Length;
+}box1,box2;
 
-// Do not change the main() function
 int main() {
-   int length = 10, width = 5;
-   input(length, width);
-   print(length, width);
-   return 0;
+		 
+		int Height, Width, Length;
+		int totalVolume;
+ 
+		cout << "Enter Box 1 Height : ";
+		cin >> box1.Height;
+		cout << "Enter Box 1 Width : ";
+	    cin >> box1.Width;
+		cout << "Enter Box 1 Length : ";
+		cin >> box1.Length;
+			 
+		cout << "Enter Box 2 Height : ";
+		cin >> box2.Height;
+		cout << "Enter Box 2 Width : ";
+		cin >> box2.Width;
+		cout << "Enter Box 2 Length : ";
+		cin >> box2.Length;
+			 
+		totalVolume = volume(box1.Height, box1.Width, box1.Length)
+			         + volume(box2.Height, box2.Width, box2.Length);
+			 
+		cout << "Volume of Box is " << totalVolume << endl;
+			 
+ return 0;
 }
-
-// Do not change the print() function
-void print(int len, int wth) {
-   cout << "Length : " << len 
-        << ", Width  : " << wth << endl;
+int volume(int height, int width, int length)
+{
+	return height * width * length;
 }
-
-// Implement the Input Function here
